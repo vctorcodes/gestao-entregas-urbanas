f08a6fd47fecd08937ac39f6843f601b7a39f75e
Victor <128561046+vctorcodes@users.noreply.github.com>
2026-01-21 21:02:22 -0300

    First commit

---
+++ src/main/java/com/example/gestaoEntregasUrbanas/GestaoEntregasUrbanasApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestaoEntregasUrbanas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoEntregasUrbanasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoEntregasUrbanasApplication.class, args);
+	}
+
+}
